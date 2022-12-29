5c4efe52bb7d511179122ada1bc293050ef08222
gepiko <aoriaira@gmail.com>
2022-12-29 13:34:32 +0300

    added custom validationPipe for error message separation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common/pipes';
+// import { ValidationPipe } from '@nestjs/common/pipes';
+import { ValidationPipe } from './myValidationPipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
