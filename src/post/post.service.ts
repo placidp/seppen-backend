211787cb15731ba3ac24d7705c5329044987bc64
gepiko <aoriaira@gmail.com>
2022-12-30 20:25:57 +0300

    feat: added post search & increase view count

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
+import { SearchPostDto } from './dto/search-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PostEntity } from './entities/post.entity';
 
@@ -38,14 +39,50 @@ export class PostService {
     };
   }
 
-  async findOne(id: number) {
-    const found = await this.repository.findOne({ where: { id } });
+  async search(dto: SearchPostDto) {
+    const qb = this.repository.createQueryBuilder('p');
 
-    if (!found) {
-      throw new NotFoundException('Статья не найдена');
+    qb.limit(dto.limit || 8);
+
+    if (dto.views) {
+      qb.orderBy('views', dto.views);
     }
 
-    return found;
+    if (dto.body) {
+      qb.andWhere(`p.body ILIKE :body`);
+    }
+
+    if (dto.title) {
+      qb.andWhere(`p.title ILIKE :title`);
+    }
+
+    if (dto.tag) {
+      qb.andWhere(`p.tags ILIKE :tag`);
+    }
+
+    qb.setParameters({
+      title: `%${dto.title}%`,
+      body: `%${dto.body}%`,
+      tag: `%${dto.tag}%`,
+      views: dto.views || '',
+    });
+
+    const [items, total] = await qb.getManyAndCount();
+
+    return { items, total };
+  }
+
+  async findOne(id: number) {
+    await this.repository
+      .createQueryBuilder('posts')
+      .whereInIds(id)
+      .update()
+      .set({
+        views: () => 'views + 1',
+      })
+      .execute();
+
+    return this.repository.findOne({ where: { id } });
   }
 
   async update(id: number, dto: UpdatePostDto) {
