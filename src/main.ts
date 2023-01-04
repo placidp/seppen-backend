05a403d93dba8777125af6cc98ca41113bc469e0
gepiko <aoriaira@gmail.com>
2023-01-05 00:05:23 +0300

    added cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,16 @@ import { ValidationPipe } from './myValidationPipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [/^(.*)/],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+    credentials: true,
+    allowedHeaders:
+      'Origin,X-Requested-With,Content-Type,Accept,Authorization,authorization,X-Forwarded-for',
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(7777);
 }
